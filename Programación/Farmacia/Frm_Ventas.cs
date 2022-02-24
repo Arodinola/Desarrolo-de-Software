0d30f5544bbcebfa97248962f2cae5c46cef47c6
JoseAbrego1 <99070853+JoseAbrego1@users.noreply.github.com>
2022-02-24 01:10:56 -0600

    Revert "Revert "Formularios""
    This reverts commit e37fe516ebb45bfa049a693b5918b8ca7d98a21c.

---
+++ Programación/Farmacia/Frm_Ventas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farmacia
+{
+    public partial class Frm_Ventas : Form
+    {
+        public Frm_Ventas()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ventas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvDetalleProducto_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
